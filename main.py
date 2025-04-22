76af79a620f59e55e2fccc6ae9f38ae570bcd2d1
faghiharff <46990733+faghiharff@users.noreply.github.com>
2025-04-22 20:48:02 +0330

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from telethon.sync import TelegramClient
 from telethon import events
 import os
+from flask import Flask
+import threading
 
-# اطلاعات ربات از متغیرهای محیطی
+# سرور Flask برای health check
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "Robot is running!", 200
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8000)  # پورت 80 برای health check
+
+# اطلاعات ربات
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 phone = os.getenv('PHONE_NUMBER')
@@ -10,7 +22,7 @@ source_chat = os.getenv('SOURCE_CHAT_ID')
 destination_chat = os.getenv('DESTINATION_CHAT_ID')
 
 # لیست کلمات کلیدی
-keywords = ['entry1', 'entry']  # کلمات مورد نظرتون
+keywords = ['entry1' , 'entry']  # کلمات مورد نظرتون
 
 # ایجاد کلاینت تلگرام
 client = TelegramClient('session', api_id, api_hash)
@@ -32,5 +44,10 @@ async def main():
 
     await client.run_until_disconnected()
 
+# اجرای Flask در نخ جداگانه
+flask_thread = threading.Thread(target=run_flask)
+flask_thread.start()
+
+# اجرای ربات
 with client:
     client.loop.run_until_complete(main())
